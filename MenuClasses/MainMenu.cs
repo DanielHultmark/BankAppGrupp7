7b70f29c87e4f2f63c2ae24ea7638669dd715fb2
Oscar Norén <oscar.noren02@gmail.com>
2025-10-23 14:12:18 +0200

    MainMenu and the basic method has been created and works.
    Also made a test of inputvalidation for the string choice and it works

---
+++ MenuClasses/MainMenu.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAppGrupp7.MenuClasses
+{
+    internal class MainMenu
+    {
+        public void DisplayMainMenu()
+        {
+            bool isRunning = true;
+            while (isRunning)
+            {
+                Console.WriteLine("Välkommen till BankAppGrupp7! Vill du \n1. Logga in\n2. Avsluta programmet");
+                string? choice = InputValidation.TrimmedString();
+                switch (choice) 
+                {
+                    case "1":
+                        //LoginMenu here
+                        break;
+
+                    case "2":
+                        Console.WriteLine("Avslutar program...");
+                        Thread.Sleep(5000);
+                        isRunning = false;
+                        break;
+                }
+            }
+        }
+       
+    }
+}
