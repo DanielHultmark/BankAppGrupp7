a42a5befdf07d3f8f79904500b1d027eadaf338b
Oscar Norén <oscar.noren02@gmail.com>
2025-10-23 13:27:20 +0200

    Copied the changes from main to my BankAppGrupp7.cs, hope it doesn't destroy the project
    Added InputValidation and made methods for Double and TrimmedString with added comments

---
+++ MenuClasses/InputValidation.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAppGrupp7.MenuClasses
+{
+    public class InputValidation
+    {
+        //Input validation for double
+        public static double Double()
+        {
+            double value = 0;
+            bool wasSuccesful = false;
+            bool stillValidating = true;
+
+            while (stillValidating)
+            {
+                string? userInput = Console.ReadLine();
+                //Checks if there is something inside the string and that it isn't empty including just a space
+                if (!string.IsNullOrWhiteSpace(userInput))
+                {
+                    //Trim removes specific characters 
+                    string trimmedUserInput = userInput.Trim();
+                    //If the input is valid the bool will become successful
+                    wasSuccesful = double.TryParse(trimmedUserInput, out value);
+                }
+
+                if (wasSuccesful) 
+                {
+                    //If the input is valid then stillValidating becomes false, ending the loop
+                    stillValidating = false;
+                }
+                else
+                {
+                    //If the input isn't valid then the loop restarts
+                    Thread.Sleep(1000);
+                    Console.WriteLine("Välj ett nummer, försök igen!");
+                }
+
+
+            }
+            return value;
+        }
+
+        public static string TrimmedString()
+        {
+            string value = "";
+            bool wasSuccesful = false;
+            bool stillValidating = true;
+            while (stillValidating)
+            {
+                string? userInput = Console.ReadLine();
+                //Trims the string if the string isn't empty or a " " string
+                if (!string.IsNullOrWhiteSpace(userInput)) 
+                {
+                    value = userInput.Trim();
+                    wasSuccesful = true;
+                }
+
+                if (wasSuccesful)
+                {
+                    stillValidating = false;
+                }
+                else
+                {
+                    Thread.Sleep(1000);
+                    Console.WriteLine("Välj rätt input, försök igen!");
+                }
+            }
+
+            return value;
+        }
+    }
+}
