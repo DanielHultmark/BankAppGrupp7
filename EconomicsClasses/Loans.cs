e007592ad73cc6806d2db5ae37e718308e7704d3
Daniel Hultmark <danielhultmark@gmail.com>
2025-10-27 09:57:04 +0100

    Completed method ApplyForLoan

--- EconomicsClasses/Loans.cs
+++ EconomicsClasses/Loans.cs
@@ -10,18 +10,18 @@ namespace BankAppGrupp7.EconomicsClasses
     internal class Loans
     {
         public List<Users> User { get; set; }
-        public double Amount { get; set; }
-        public double InterestRate { get; set; }  
-        public double LengthOfLoan { get; set; }
+        public decimal Amount { get; set; }
+        public decimal InterestRate { get; set; }  
+        public decimal LengthOfLoan { get; set; }
 
-        public Loans(List<Users> user, double amount, double interestRate, double lengthOfLoan)
+        public Loans(List<Users> user, decimal amount, decimal interestRate, decimal lengthOfLoan)
         {
             User = user;
             Amount = amount;
             InterestRate = interestRate;
             LengthOfLoan = lengthOfLoan;
 
-            interestRate = 0.0254; // Standard ränta på 5%
+            interestRate = 0.0254M; // Standard ränta på 5%
 
             List<Loans> listOfLoans = new List<Loans>();            
         }
@@ -29,13 +29,25 @@ namespace BankAppGrupp7.EconomicsClasses
         public void ApplyForLoan()
         {
             Console.WriteLine("Hur mycket önskar du att låna?");
-            double amount = Convert.ToDouble(Console.ReadLine());
-            double total = amount * InterestRate / 100;
-            Console.WriteLine("Under hur lånt tid önskar du att betala tillbaka lånet?");
-            int years = int.TryParse(Console.ReadLine());
-            Console.WriteLine($"Du har ansökt om ett lån på {amount} kr med en ränta på {InterestRate}%. Totalt att återbetala är {total} kr.");
-            Loans newLoan = new Loans(this.User, amount, InterestRate);
             
+            
+            Console.WriteLine("Under hur lånt tid önskar du att betala tillbaka lånet?");
+            if (!decimal.TryParse(Console.ReadLine(), out decimal amount))
+            {
+                Console.WriteLine("Felatkig inmatning, försök igen!");
+                return;
+            }
+            if (!decimal.TryParse(Console.ReadLine(), out decimal lengthOfLoan))
+            {
+                Console.WriteLine("Felaktig inmatning, försök igen!");
+                return;
+            }
+            decimal intrest = amount * InterestRate / 100;
+            decimal total = amount * InterestRate / 100;
+            Console.WriteLine($"Du har ansökt om ett lån på {amount} kr med en ränta på {InterestRate}%. Totalt att återbetala är {total} kr.");
+            Loans newLoan = new Loans(this.User, amount, InterestRate, lengthOfLoan);
+            //Lägg till lånet i listan över lån
+
         }
     }
 }
