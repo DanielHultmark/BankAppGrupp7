1be625278cc18928d3bfa0c04a4db8e0c9f6d889
Eric Dahlgren <eric_dahlgren_@hotmail.com>
2025-10-27 10:39:58 +0100

    accounts

---
+++ AccountClasses/SavingsAccount.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using BankAppGrupp7.EconomicsClasses; 
+
+namespace BankAppGrupp7.AccountClasses
+{
+    public class SavingsAccount : Account
+    {
+        public decimal InterestRate { get; set; }
+
+        public SavingsAccount(string accountNumber, Customer owner, decimal balance, Currency currency, decimal interestRate)
+            : base(accountNumber, owner, balance, currency)
+        {
+            InterestRate = interestRate;
+        }
+
+        public override void Deposit(decimal amount)
+        {
+            Balance += amount;
+            new Transaction((decimal)amount, this, this); 
+        }
+
+        public override void Withdraw(decimal amount)
+        {
+            if (amount <= Balance)
+            {
+                Balance -= amount;
+                new Transaction((double)amount, this, this);
+            }
+            else
+            {
+                Console.WriteLine("Otillräckligt saldo.");
+            }
+        }
+    }
+}
