206ad1e6f2080f0d8ed763234328e4e818d45f32
Daniel Hultmark <danielhultmark@gmail.com>
2025-10-27 11:23:51 +0100

    Added Erics Classes

---
+++ AccountClasses/Accounts.cs
@@ -0,0 +1,39 @@
+﻿using BankAppGrupp7.EconomicsClasses;
+using BankAppGrupp7.UsersClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace BankAppGrupp7.AccountClasses
+{
+    internal class Accounts
+    {
+        public abstract class Account
+        {
+            public string AccountNumber { get; set; }
+            public Customer Owner { get; set; }
+            public decimal Balance { get; set; }
+            public Currency Currency { get; set; }
+            public List<Transaction> Transactions { get; set; } = new List<Transaction>();
+
+            public Account(string accountNumber, Customer owner, decimal balance, Currency currency)
+            {
+                AccountNumber = accountNumber;
+                Owner = owner;
+                Balance = balance;
+                Currency = currency;
+            }
+
+            public void ShowBalance()
+            {
+                Console.WriteLine($"Saldo för konto {AccountNumber}: {Balance} {Currency}");
+            }
+
+            public abstract void Deposit(decimal amount);
+            public abstract void Withdraw(decimal amount);
+        }
+    }
+}
---
+++ AccountClasses/SavingAccounts.cs
@@ -0,0 +1,45 @@
+﻿using BankAppGrupp7.EconomicsClasses;
+using BankAppGrupp7.UsersClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+using static BankAppGrupp7.AccountClasses.Accounts;
+
+namespace BankAppGrupp7.AccountClasses
+{
+    internal class SavingAccounts
+    {
+        public class SavingsAccount : Account
+        {
+            public decimal InterestRate { get; set; }
+
+            public SavingsAccount(string accountNumber, Customer owner, decimal balance, Currency currency)
+                : base(accountNumber, owner, balance, currency)
+            {
+                InterestRate = 1.005M;
+            }
+
+            public override void Deposit(decimal amount)
+            {
+                Balance += amount;
+                new Transaction((decimal)amount, this, this);
+            }
+
+            public override void Withdraw(decimal amount)
+            {
+                if (amount <= Balance)
+                {
+                    Balance -= amount;
+                    new Transaction((double)amount, this, this);
+                }
+                else
+                {
+                    Console.WriteLine("Otillräckligt saldo.");
+                }
+            }
+        }
+    }
+}
