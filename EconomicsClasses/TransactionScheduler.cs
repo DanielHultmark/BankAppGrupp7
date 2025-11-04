63420ae23faee4db6df3f1807306053c18c1f14e
Katarina Holm <katarinaholm89@gmail.com>
2025-11-04 10:49:51 +0100

    Draft to Schedule Transactions

---
+++ EconomicsClasses/TransactionScheduler.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using BankAppGrupp7.AccountClasses;
+using BankAppGrupp7.MenuClasses;
+
+namespace BankAppGrupp7.EconomicsClasses
+{
+    public class TransactionScheduler
+    {
+        Queue<Transaction> pendingTransactions { get; set; } = new Queue<Transaction>();
+
+        public void AddPendingTransaction(decimal amount, Account fromAccount, Account toAccount)
+        {             
+            Transaction pendningTransaction = new Transaction(amount, fromAccount, toAccount);
+
+            pendingTransactions.Enqueue(pendningTransaction);
+
+        }
+        // OM kl23?!
+        public async Task Start() 
+        {
+            DateTimeOffset now = DateTimeOffset.Now;
+            int nextQuarterMinute = ((now.Minute / 15) + 1) * 15;
+            
+            if (nextQuarterMinute == 60)
+            {
+                nextQuarterMinute = 0;
+                now = now.AddHours(1);
+
+                if (now.Hour==0)
+                {
+                    now.AddDays(1);
+                }
+            }
+
+            DateTimeOffset nextQuarter = new DateTimeOffset(now.Year, now.Month, now.Day, now.Hour, nextQuarterMinute, 0, now.Offset);
+            
+            TimeSpan timeToStartTimer= nextQuarter - DateTimeOffset.Now;                      
+
+            var quarterTimer = new Timer(ExecuteTransactions, null, timeToStartTimer, TimeSpan.FromMinutes(15));
+
+        }
+               
+
+        public void ExecuteTransactions(object? state)
+        {
+            if (pendingTransactions.Count > 0)
+            {
+                CurrencyConversion currencyConversion = new CurrencyConversion();
+                
+                foreach (var transaction in pendingTransactions)
+                {
+                    BankRegister bankRegister = new BankRegister();
+                    bankRegister.AddTransaction(pendingTransactions.Dequeue());
+                    decimal convertedAmount = currencyConversion.ConvertCurrency(transaction.Amount, transaction.FromAccount.Currency, transaction.ToAccount.Currency);
+
+                    transaction.FromAccount.Balance -= transaction.Amount;
+                    transaction.ToAccount.Balance += convertedAmount;
+
+                }
+            }
+        }
+
+    }
+}
